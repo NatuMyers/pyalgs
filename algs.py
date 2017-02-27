13f3706de724c5b7d193498bd560c26cbada443c
Natu Myers <me@natumyers.com>
2017-02-26 19:59:51 -0500

    Update algs.py

--- algs.py
+++ algs.py
@@ -5,21 +5,31 @@ def reverse(str):
 
 
 # http://www.programcreek.com/2015/03/rotate-array-in-java/
+# helpful:
+# http://stackoverflow.com/questions/17911091/append-integer-to-beginning-of-list-in-python
 def rotate(arr,k):
-    if k == 0 or len(arr) < 2:
+  
+    if k < 1 or len(arr) < 2:
         return arr
+        
     else:
+      
         # if we don't iterate with a number, but with an array,
         # let's try a while loop
-
  
         while k > 0:
-            last_ele = arr[-2:-1] # -2 is second last, -1 is last
-            arr = arr[:-1] # ignore the last element by triming it off
-            arr.append(last_ele) # append puts stuff at FRONT
+          
+            last_ele = [arr[-1]] # http://stackoverflow.com/questions/930397/getting-the-last-element-of-a-list-in-python
+            
+            
+            # both: [-2:-1] gives you the last element.
+
+            shortened_arr = arr[:-1] # ignore the last element by triming it off
+            
+            arr = last_ele + shortened_arr
 
             k = k-1
 
     return arr
-
-print (rotate([1,2,3,4],2))
+    
+  
