0692e54718333fa9f7dd9dc5e36c554948980d22
Natu Myers <me@natumyers.com>
2017-02-26 20:01:03 -0500

    Update algs.py

--- algs.py
+++ algs.py
@@ -13,23 +13,14 @@ def rotate(arr,k):
         return arr
         
     else:
-      
         # if we don't iterate with a number, but with an array,
         # let's try a while loop
  
         while k > 0:
-          
             last_ele = [arr[-1]] # http://stackoverflow.com/questions/930397/getting-the-last-element-of-a-list-in-python
-            
-            
             # both: [-2:-1] gives you the last element.
-
             shortened_arr = arr[:-1] # ignore the last element by triming it off
-            
             arr = last_ele + shortened_arr
-
             k = k-1
 
     return arr
-    
-  
